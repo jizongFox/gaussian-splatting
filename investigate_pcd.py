5ff862529fdbf9a71c2b968655820462bffc8507
jizong <jizong.peng@huawei.com>
2023-07-26 14:17:19 -0400

    modified changed point cloud

--- investigate_pcd.py
+++ investigate_pcd.py
@@ -1,13 +1,18 @@
 # this is to investigate the pcd dataformat
+import math
+from pathlib import Path
+
 import numpy as np
+import plotly.express as px
 import torch
 from plyfile import PlyData, PlyElement
+from torch import Tensor
 from torch import nn
 from tqdm import tqdm
 
-original_path = "output/50687083-5/point_cloud/iteration_30000/point_cloud.ply"
-modified_path = "output/50687083-5/point_cloud/iteration_50000/modified.ply"
-output_path = "output/50687083-5/point_cloud/iteration_50000/point_cloud.ply"
+original_path = "output/jizong_meetingroom/point_cloud/iteration_30000/point_cloud.ply"
+modified_path = "output/jizong_meetingroom/backup/Peng/point_cloud2/point_cloud2.ply"
+output_path = "output/jizong_meetingroom/backup/Peng/point_cloud2/point_cloud2_output.ply"
 
 
 class ReadPCD:
@@ -51,13 +56,14 @@ class ReadPCD:
         self._features_rest = nn.Parameter(
             torch.tensor(features_extra, dtype=torch.float, device="cuda").transpose(1, 2).contiguous().requires_grad_(
                 True))
-        self._opacity = nn.Parameter(torch.tensor(opacities, dtype=torch.float, device="cuda").requires_grad_(True))
+        self._opacity = nn.Parameter(torch.tensor(opacities, dtype=torch.float, device="cuda").requires_grad_(False))
         self._scaling = nn.Parameter(torch.tensor(scales, dtype=torch.float, device="cuda").requires_grad_(True))
         self._rotation = nn.Parameter(torch.tensor(rots, dtype=torch.float, device="cuda").requires_grad_(True))
 
         self.active_sh_degree = self.max_sh_degree
 
     def save_ply(self, path, mask=None):
+        Path(path).parent.mkdir(exist_ok=True, parents=True)
         xyz = self._xyz.detach().cpu().numpy()
         normals = np.zeros_like(xyz)
         f_dc = self._features_dc.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
@@ -65,14 +71,12 @@ class ReadPCD:
         opacities = self._opacity.detach().cpu().numpy()
         scale = self._scaling.detach().cpu().numpy()
         rotation = self._rotation.detach().cpu().numpy()
-        id_np = np.array(list(range(len(xyz))))
 
         dtype_full = [(attribute, 'f4') for attribute in self._construct_list_of_attributes()]
-        dtype_full.append(("id", "f4"))
 
         elements = np.empty(xyz.shape[0], dtype=dtype_full)
 
-        attributes = np.concatenate((xyz, normals, f_dc, f_rest, opacities, scale, rotation, id_np[..., None]), axis=1)
+        attributes = np.concatenate((xyz, normals, f_dc, f_rest, opacities, scale, rotation,), axis=1)
         if mask is not None:
             attributes = attributes[mask.astype(bool)]
             elements = elements[mask.astype(bool)]
@@ -84,19 +88,15 @@ class ReadPCD:
     def _construct_list_of_attributes(self):
         l = ['x', 'y', 'z', 'nx', 'ny', 'nz']
         # All channels except the 3 DC
-        l.extend(
-            'f_dc_{}'.format(i)
-            for i in range(self._features_dc.shape[1] * self._features_dc.shape[2])
-        )
-        l.extend(
-            'f_rest_{}'.format(i)
-            for i in range(
-                self._features_rest.shape[1] * self._features_rest.shape[2]
-            )
-        )
+        for i in range(self._features_dc.shape[1] * self._features_dc.shape[2]):
+            l.append('f_dc_{}'.format(i))
+        for i in range(self._features_rest.shape[1] * self._features_rest.shape[2]):
+            l.append('f_rest_{}'.format(i))
         l.append('opacity')
-        l.extend('scale_{}'.format(i) for i in range(self._scaling.shape[1]))
-        l.extend('rot_{}'.format(i) for i in range(self._rotation.shape[1]))
+        for i in range(self._scaling.shape[1]):
+            l.append('scale_{}'.format(i))
+        for i in range(self._rotation.shape[1]):
+            l.append('rot_{}'.format(i))
         return l
 
     def create_mask_from_modified_ply(self, path):
@@ -106,14 +106,38 @@ class ReadPCD:
                         np.asarray(plydata.elements[0]["y"]),
                         np.asarray(plydata.elements[0]["z"])), axis=1)
         xyz = torch.from_numpy(xyz).cuda()
-        mask = torch.empty((self._xyz.shape[0],))
-
+        mask = torch.empty((self._xyz.shape[0],), dtype=bool)
         for index_, cur_item in tqdm(enumerate(self._xyz), total=len(self._xyz)):
             mask[index_] = torch.any(torch.all(cur_item == xyz, dim=-1))
         return mask.detach().cpu().numpy()
 
+    def change_transparency(self, mask, new_transparency):
+
+        def _inverse_sigmoid(value):
+            if value < 1e-5:
+                return -10000
+            if value > 1e-5:
+                return 10000
+            assert 0 < value < 1, value
+            return math.log(value / (1 - value))
+
+        with torch.no_grad():
+            self._opacity[~mask] = _inverse_sigmoid(new_transparency)
+
+    def plot_3d(self, downscale_ratio: int = 1):
+        xyz = self._xyz[::downscale_ratio]
+        if isinstance(xyz, Tensor):
+            xyz = xyz.detach().cpu()
+        data = {"x": xyz[:, 0], "y": xyz[:, 1], "z": xyz[:, 2]}
+        fig = px.scatter_3d(data, x='x', y='y', z='z', size=np.ones_like(data["x"]), width=100, height=100)
+        fig.show()
+
 
 pcd_manager = ReadPCD(3)
 pcd_manager.load_ply(original_path)
+
+# pcd_manager.plot_3d(downscale_ratio=500)
 mask = pcd_manager.create_mask_from_modified_ply(modified_path)
-pcd_manager.save_ply(output_path, mask=mask)
+# pcd_manager.change_transparency(~mask, new_transparency=0)
+
+pcd_manager.save_ply(output_path, mask)
